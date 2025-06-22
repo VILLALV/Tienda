39795f24417d1f5a9208b2a5f102d98f1d488fe2
IanVillalobos <ianvillalobosalvarez@gmail.com>
2025-06-22 01:28:45 -0600

    Semana6

--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,4 +1,3 @@
-
 package com.tienda.service.impl;
 
 import com.tienda.dao.CategoriaDao;
@@ -7,28 +6,39 @@ import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-//implementa la lógica de los métodos de la  interfaz que implementa
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
 
-    
-    
-    //Infección
     @Autowired
     private CategoriaDao categoriaDao;
-    
+
     @Override
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
         List<Categoria> lista = categoriaDao.findAll();
-        
-        //Filtra solo los activos
-        if (activos){
-            lista.removeIf(cat -> !cat.isActivo());
-
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
-        
     }
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.deleteById(categoria.getIdCategoria());
+    }
 }
